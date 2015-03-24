692f65114e57a04c7f660a56ac12c179b4323c05
blank <blank@moss-12.local>
2015-03-24 22:22:21 +1100

    Creates orientation, position, a move forward method and some supporting methods.

--- lib/robocop.rb
+++ lib/robocop.rb
@@ -1,5 +1,72 @@
+# Make sure no 'new' headings can be made?
+module Heading
+  :north
+  :south
+  :east
+  :west
+end
+
 class Robocop
-    def beep
-        'beep boop'
+
+  def initialize()
+    @heading = :north
+    @position = [0, 0]
+  end
+
+
+  def beep
+    'beep boop'
+  end
+
+  def can_move_forward?
+    true
+  end
+
+  ##
+  # Assume that the positive second element is north, and positive first element is east.
+  # This deserve refactoring so that it just passes a 'velocity' vector to a general move method.
+  def move_forward
+    old_position = get_position
+    case
+      when @heading == :north
+        set_position(old_position[0], old_position[1]+1)
+      when @heading == :south
+        set_position(old_position[0], old_position[1]-1)
+      when @heading == :east
+        set_position(old_position[0]+1, old_position[1])
+      when @heading == :west
+        set_position(old_position[0]-1, old_position[1])
     end
+    get_position
+  end
+
+  def set_heading(headed)
+    @heading = headed
+  end
+
+  def get_heading
+    return @heading
+  end
+
+  def get_position
+    @position
+  end
+
+  def set_position(xy_array)
+    xy_array.is_a?(Array)
+    xy_array.array.size.must_equal(2)
+    xy_array.all? { |i| i.is_a?(Numeric)}
+  end
+
+  def set_position(x_position, y_position)
+    if x_position.is_a?(Numeric) and y_position.is_a?(Numeric)
+      @position = [x_position, y_position]
+      return true
+    else
+      false
+    end
+  end
+
+
+
 end
--- spec/robocop_spec.rb
+++ spec/robocop_spec.rb
@@ -1,8 +1,91 @@
 require 'robocop'
 
 RSpec.describe Robocop do
+  let(:d) {Robocop.new}
+
   it "should beep" do
-    d = Robocop.new
     expect(d.beep).to eq('beep boop')
   end
+
+  it "can move forward" do
+    expect(d.can_move_forward?).to eq(true)
+  end
+
+  it "moves north if facing north" do
+    old_position = d.get_position
+    orientation = d.get_heading
+    if orientation == :north
+      d.move_forward
+      expect(d.get_position[1]).to eq(old_position[1] + 1)
+      expect(d.get_position[0]).to eq(old_position[0])
+    end
+
+  end
+
+  it "moves south if facing south" do
+    old_position = d.get_position
+    orientation = d.get_heading
+    if orientation == :south
+      d.move_forward
+      expect(d.get_position[1]).to eq(old_position[1] - 1)
+      expect(d.get_position[0]).to eq(old_position[0])
+    end
+
+  end
+
+  it "moves east if facing east" do
+    old_position = d.get_position
+    orientation = d.get_heading
+    if orientation == :east
+      d.move_forward
+      expect(d.get_position[1]).to eq(old_position[1])
+      expect(d.get_position[0]).to eq(old_position[0] - 1)
+    end
+
+  end
+
+  it "moves west if facing west" do
+    old_position = d.get_position
+    orientation = d.get_heading
+    if orientation == :west
+      d.move_forward
+      expect(d.get_position[1]).to eq(old_position[1])
+      expect(d.get_position[0]).to eq(old_position[0] + 1)
+    end
+
+  end
+
+  it "has a heading" do
+    expect(d.get_heading.nil?).to eq(false)
+  end
+
+  it "is facing north" do
+    d.set_heading(:north)
+    expect(d.get_heading).to eq(:north)
+  end
+
+  it "is facing south" do
+    d.set_heading(:south)
+    expect(d.get_heading).to eq(:south)
+  end
+
+  it "is facing west" do
+    d.set_heading(:west)
+    expect(d.get_heading).to eq(:west)
+  end
+
+  it "is facing east" do
+    d.set_heading(:east)
+    expect(d.get_heading).to eq(:east)
+  end
+
+  it "gives a valid position" do
+    expect(d.get_position).to be_a(Array)
+    expect(d.get_position.size).to eq(2)
+    expect(d.get_position.first.is_a?(Numeric))
+    expect(d.get_position.last.is_a?(Numeric))
+  end
+
+
+
 end
